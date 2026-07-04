96434902d888057d002d322645ca602e1a667707
kmg <kmgccc@outlook.com>
2026-07-04 21:22:48 +0800

    chore: specify verified PaddleOCR stack in requirements and add runtime environment variables

--- scripts/run_on_car.sh
+++ scripts/run_on_car.sh
@@ -32,6 +32,6 @@ fi
 if [ '$RUN_MODE' = 'robot' ] && [ -x /home/pi/project_demo/raspbot/killprocess.sh ]; then
   /bin/sh /home/pi/project_demo/raspbot/killprocess.sh || true
 fi
-RUN_MODE='$RUN_MODE' AUTO_START_RUNTIME='$AUTO_START_RUNTIME' LINE_FOLLOW_ENABLED='$LINE_FOLLOW_ENABLED' PORT='$PORT' nohup \"\$PYTHON_CMD\" app.py > app.log 2>&1 & echo \$!
+RUN_MODE='$RUN_MODE' AUTO_START_RUNTIME='$AUTO_START_RUNTIME' LINE_FOLLOW_ENABLED='$LINE_FOLLOW_ENABLED' PORT='$PORT' FLAGS_use_mkldnn=0 OMP_NUM_THREADS=1 CPU_NUM=1 nohup \"\$PYTHON_CMD\" app.py > app.log 2>&1 & echo \$!
 "
 echo "Started on $CAR_HOST:$CAR_DIR with RUN_MODE=$RUN_MODE, AUTO_START_RUNTIME=$AUTO_START_RUNTIME, LINE_FOLLOW_ENABLED=$LINE_FOLLOW_ENABLED, PORT=$PORT"
